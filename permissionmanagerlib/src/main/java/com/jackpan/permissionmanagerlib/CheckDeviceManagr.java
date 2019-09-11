dc803ecdac3a83dcfdc0f5bb9a92b26b31fa4f03
wei0820 <jheng.wei0919@gmail.com>
2019-09-11 11:47:40 +0800

    fix fun

--- permissionmanagerlib/src/main/java/com/jackpan/permissionmanagerlib/CheckDeviceManagr.java
+++ permissionmanagerlib/src/main/java/com/jackpan/permissionmanagerlib/CheckDeviceManagr.java
@@ -54,7 +54,7 @@ public class CheckDeviceManagr {
                  */
                 // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
                 ActivityCompat.requestPermissions(
-                        this,
+                        ((Activity)context),
                         new String[]{
                                 Manifest.permission.READ_PHONE_STATE,
                                 Manifest.permission.ACCESS_COARSE_LOCATION,
