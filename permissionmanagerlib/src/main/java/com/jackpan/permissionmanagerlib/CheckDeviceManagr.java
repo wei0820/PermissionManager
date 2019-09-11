92f95bf85b0ef99f3d8ffa03480391d4f09f0165
wei0820 <jheng.wei0919@gmail.com>
2019-09-11 11:16:44 +0800

    add function

--- permissionmanagerlib/src/main/java/com/jackpan/permissionmanagerlib/CheckDeviceManagr.java
+++ permissionmanagerlib/src/main/java/com/jackpan/permissionmanagerlib/CheckDeviceManagr.java
@@ -1,16 +1,20 @@
 package com.jackpan.permissionmanagerlib;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.Settings;
+
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -107,6 +111,42 @@ public class CheckDeviceManagr {
         } else {
             return false;
         }
+    }
+
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private boolean checkPermissionAllGranted(Context context,String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 检测权限
+     *
+     * @return true 所需权限全部授取  false 存在未授权的权限
+     */
+    public boolean isAllGranted(Context context) {
+        /**
+         * 第 1 步: 检查是否有相应的权限
+         */
+        boolean isAllGranted = checkPermissionAllGranted(context,
+                new String[]{
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }
+        );
+
+        return isAllGranted;
     }
 
     /**
