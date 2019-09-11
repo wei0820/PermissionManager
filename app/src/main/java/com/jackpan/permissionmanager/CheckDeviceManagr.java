4e651862a86a2d2e1f3b363559c834ee572defbf
wei0820 <jheng.wei0919@gmail.com>
2019-09-11 10:47:46 +0800

    add fun

--- app/src/main/java/com/jackpan/permissionmanager/CheckDeviceManagr.java
+++ app/src/main/java/com/jackpan/permissionmanager/CheckDeviceManagr.java
@@ -1,6 +1,52 @@
 package com.jackpan.permissionmanager;
 
-public class CheckDeviceManagr {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 
-    
+public class CheckDeviceManagr {
+    //检测MIUI
+    private static final String KEY_MIUI_VERSION_CODE = "ro.miui.ui.version.code";
+    private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
+    private static final String KEY_MIUI_INTERNAL_STORAGE = "ro.miui.internal.storage";
+    //系统授权设置的弹框
+    AlertDialog openAppDetDialog = null;
+
+
+    /**
+     * 打开 APP 的详情设置
+     */
+    private void openAppDetails(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(context.getString(R.string.app_name) + "需要访问 \"设备信息\"、\"相册\"、\"定位\" 和 \"外部存储器\",请到 \"应用信息 -> 权限\" 中授予！");
+        builder.setPositiveButton("手动授权", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + context.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                context.startActivity(intent);
+            }
+        });
+        builder.setCancelable(false);
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ((Activity)context).finish();
+            }
+        });
+        if (null == openAppDetDialog)
+            openAppDetDialog = builder.create();
+        if (null != openAppDetDialog && !openAppDetDialog.isShowing())
+            openAppDetDialog.show();
+    }
+
 }
